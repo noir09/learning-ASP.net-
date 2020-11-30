ad8b93b4349042ed14cd6f26d695c5cfdb972ff2
Nikita <aldnor09@gmail.com>
2020-11-30 17:27:09 +0300

    CHECK

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string str;
+            Console.WriteLine("hello world!");
+        }
+    }
+}
